22f5108ee0713f5552bf317a38203bfad2c43fe7
matthew-dipierro <matthew.dipierro@gmail.com>
2025-09-18 13:49:54 -0400

    Create db-connection.js

---
+++ backend/db-connection.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+// Use DATABASE_URL from Render PostgreSQL service
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
+
+module.exports = pool;
